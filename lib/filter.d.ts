979d632d91fc00d828c96db1d4e0f0d6ec9722c2
Andreas Scheck <andreas.scheck@mercatis.com>
2017-02-16 09:25:15 +0100

    switched to vue2

---
+++ lib/filter.d.ts
@@ -0,0 +1,2 @@
+export declare function VueFilter(options: vuejs.FilterOption): any;
+export declare function VueFilter(target: Object, key: string): any;
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/clone/index.d.ts" />
+/// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/vue/index.d.ts" />
